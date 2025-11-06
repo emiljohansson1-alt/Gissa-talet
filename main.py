aef1207082cb521cc976ab9b601f37e2a3ac710a
edu-emijoh <emil.johansson1@skola.taby.se>
2025-11-06 12:55:45 +0100

    Slumpat tal och gissning

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import random
+
+
 name = input("Vad heter du? ")
 print(f"Hej, {name}, och välkommen till spelet!")
+
+#Del 2
+
+number = random.randint(1, 10)
+guess = int(input("Gissa ett nummer mellan 1 och 10: "))
+if guess == number:
+    print("Rätt gissat!")
+else:
+    print(f"Fel gissat! Rätt nummer var {number}.")
+
+#Del 3
+
